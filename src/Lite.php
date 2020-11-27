62102c0ae6e3020e3f6b8a91d38f1edbf4ce7870
JamesLiao <vivlonglz@gmail.com>
2020-11-27 15:54:22 +0800

    style: beautify

--- src/Lite.php
+++ src/Lite.php
@@ -16,7 +16,7 @@ class Lite
         if (is_null($this->config)) {
             $this->config = \PhalApi\DI()->config->get('app.AliyunPush');
         }
-        AlibabaCloud::accessKeyClient($this->config['accessKeyId'], $this->config['accessKeySecret'])
+        AlibabaCloud:: accessKeyClient($this->config['accessKeyId'], $this->config['accessKeySecret'])
             ->regionId($this->config['regionId'])    // 设置客户端区域，使用该客户端且没有单独设置的请求都使用此设置
             ->timeout(6)                            // 超时10秒，使用该客户端且没有单独设置的请求都使用此设置
             ->connectTimeout(10)                    // 连接超时10秒，当单位小于1，则自动转换为毫秒，使用该客户端且没有单独设置的请求都使用此设置
@@ -27,41 +27,41 @@ class Lite
     public function push($pushTpye, $target, $targetValue, $deviceType, $content, $title, $extras)
     {
         $params = [
-            'PushType' => $pushTpye,
-            'DeviceType' => $deviceType,
-            'Target' => $target,
-            'TargetValue' => $targetValue,
-            'Body' => $content,
-            'Title' => $title,
+            'PushType'     => $pushTpye,
+            'DeviceType'   => $deviceType,
+            'Target'       => $target,
+            'TargetValue'  => $targetValue,
+            'Body'         => $content,
+            'Title'        => $title,
             'StoreOffline' => 'true',
             //'ExpireTime' => gmdate('Y-m-d\TH:i:s\Z', strtotime('+1 day')),
         ];
         if (strtolower($deviceType) === strtolower('ANDROID')) {
             $params = array_merge($params, [
-                'AndroidNotifyType' => 'BOTH',              //通知的提醒方式 "VIBRATE" : 震动 "SOUND" : 声音 "BOTH" : 声音和震动 NONE : 静音
-                'AndroidNotificationBarType' => '1',	        //通知栏自定义样式0-100
-                'AndroidNotificationBarPriority' => '0',      //Android通知在通知栏展示时排列位置的优先级 -2 -1 0 1 2
-                'AndroidOpenType' => 'APPLICATION',         //点击通知后动作 "APPLICATION" : 打开应用 "ACTIVITY" : 打开AndroidActivity "URL" : 打开URL "NONE" : 无跳转
-                //'AndroidOpenUrl' => '', 				    //Android收到推送后打开对应的url,仅当AndroidOpenType="URL"有效
+                'AndroidNotifyType'              => 'BOTH',          //通知的提醒方式 "VIBRATE" : 震动 "SOUND" : 声音 "BOTH" : 声音和震动 NONE : 静音
+                'AndroidNotificationBarType'     => '1',             //通知栏自定义样式0-100
+                'AndroidNotificationBarPriority' => '0',             //Android通知在通知栏展示时排列位置的优先级 -2 -1 0 1 2
+                'AndroidOpenType'                => 'APPLICATION',   //点击通知后动作 "APPLICATION" : 打开应用 "ACTIVITY" : 打开AndroidActivity "URL" : 打开URL "NONE" : 无跳转
+                //'AndroidOpenUrl' => '', 				             //Android收到推送后打开对应的url,仅当AndroidOpenType="URL"有效
                 //'AndroidActivity' => '',
-                'AndroidRemind' => 'true',                    //推送类型为消息时设备不在线，则这条推送会使用辅助弹窗功能。默认值为False，仅当PushType=MESSAGE时生效。
-                'AndroidPopupActivity' => 'com.b2b2c.app.PopupPushActivity',    //设定通知打开的activity，仅当AndroidOpenType="Activity"有效
-                'AndroidPopupTitle' => $title,
-                'AndroidPopupBody' => $content,
-                'AndroidMusic' => 'default',
-                'AndroidNotificationChannel' => $this->config['androidChannel'], //设置NotificationChannel参数
-                'AndroidExtParameters' => $extras,          // 设定android类型设备通知的扩展属性
+                'AndroidRemind'              => 'true',                              //推送类型为消息时设备不在线，则这条推送会使用辅助弹窗功能。默认值为False，仅当PushType=MESSAGE时生效。
+                'AndroidPopupActivity'       => 'com.b2b2c.app.PopupPushActivity',   //设定通知打开的activity，仅当AndroidOpenType="Activity"有效
+                'AndroidPopupTitle'          => $title,
+                'AndroidPopupBody'           => $content,
+                'AndroidMusic'               => 'default',
+                'AndroidNotificationChannel' => $this->config['androidChannel'],     //设置NotificationChannel参数
+                'AndroidExtParameters'       => $extras,                             // 设定android类型设备通知的扩展属性
             ]);
         } elseif (strtolower($deviceType) === strtolower('IOS')) {
             $params = array_merge($params, [
-                'iOSMusic' => 'default',
-                'iOSApnsEnv' => 'PRODUCT',
+                'iOSMusic'              => 'default',
+                'iOSApnsEnv'            => 'PRODUCT',
                 'iOSBadgeAutoIncrement' => 'false',
                 'iOSSilentNotification' => 'false',
-                'iOSMutableContent' => 'true',
-                'iOSRemind' => 'true',
-                'iOSRemindBody' => $content,
-                'iOSExtParameters' => $extras,
+                'iOSMutableContent'     => 'true',
+                'iOSRemind'             => 'true',
+                'iOSRemindBody'         => $content,
+                'iOSExtParameters'      => $extras,
             ]);
         }
 
@@ -71,9 +71,9 @@ class Lite
     public function bindAlias($aliasName, $deviceId)
     {
         $params = [
-            'AppKey' => $this->config['appKey'],
+            'AppKey'    => $this->config['appKey'],
             'AliasName' => $aliasName,
-            'DeviceId' => $deviceId,
+            'DeviceId'  => $deviceId,
         ];
 
         return $this->rpcRequest('BindAlias', $params);
@@ -84,7 +84,7 @@ class Lite
         $di = \PhalApi\DI();
         $rs = array(
             'code' => 0,
-            'msg' => '',
+            'msg'  => '',
             'data' => null,
         );
         try {
@@ -96,30 +96,30 @@ class Lite
                 ->host('cloudpush.aliyuncs.com')
                 ->options([
                     'query' => array_merge([
-                        'AppKey' => $this->config['appKey'],
+                        'AppKey'   => $this->config['appKey'],
                         'RegionId' => $this->config['regionId'],
                     ], $params),
                 ])
                 ->request();
             if ($result->isSuccess()) {
                 $rs['code'] = 1;
-                $rs['msg'] = 'success';
+                $rs['msg']  = 'success';
                 $rs['data'] = $result->toArray();
                 $di->logger->info('AliyunPush', 'push', array('rs' => $response['body']));
             } else {
                 $rs['code'] = -1;
-                $rs['msg'] = $result;
+                $rs['msg']  = $result;
                 $di->logger->error('AliyunPush', 'push', array('failed' => $result));
             }
         } catch (ClientException $e) {
             $di->logger->error('AliyunPush # rpcRequest', $e->getErrorMessage());
             $rs['code'] = -1;
-            $rs['msg'] = $result;
+            $rs['msg']  = $result;
             $di->logger->error('AliyunPush', 'push', array('failed' => $result));
         } catch (ServerException $e) {
             $di->logger->error('AliyunPush # rpcRequest', $e->getErrorMessage());
             $rs['code'] = -1;
-            $rs['msg'] = $result;
+            $rs['msg']  = $result;
             $di->logger->error('AliyunPush', 'push', array('failed' => $result));
         }
 
